4748331ce03f1d8d91e9b54e40171e7d5ae99926
Alexander Kozhin <a.d.kozhin@yandex.ru>
2020-11-24 16:48:43 +0300

    style: change favicon, change project title, finish searchResults layout on mobile

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -9,28 +9,38 @@ function SearchResults() {
 
         <li className='article'>
           <div className='article__image' style={{ backgroundImage: `url(${sampleImage})` }} />
-          <a className='article__body' href='#'>
-            <p className='article__date'>2 августа, 2019</p>
-            <h3 className='article__title'>Национальное достояние – парки</h3>
-            <p className='article__text'>В 2016 году Америка отмечала важный юбилей: сто лет назад здесь начала складываться система национальных парков...</p>
-            <p className='article__source'>ЛЕНТА.РУ</p>
-          </a>
+          <div className='article__wrapper'>
+            <a className='article__link' href='#' target='_blank'>
+              <p className='article__date'>2 августа, 2019</p>
+              <h3 className='article__title'>Национальное достояние – парки</h3>
+              <p className='article__text'>В 2016 году Америка отмечала важный юбилей: сто лет назад здесь начала складываться система национальных парков...</p>
+              <p className='article__source'>ЛЕНТА.РУ</p>
+            </a>
+          </div>
         </li>
 
         <li className='article'>
           <div className='article__image' style={{ backgroundImage: `url(${sampleImage})` }} />
-          <p className='article__date'>2 августа, 2019</p>
-          <h3 className='article__title'>Национальное достояние – парки</h3>
-          <p className='article__text'>В 2016 году Америка отмечала важный юбилей: сто лет назад здесь начала складываться система национальных парков...</p>
-          <p className='article__source'>МЕДУЗА</p>
+          <div className='article__wrapper'>
+            <a className='article__link' href='#' target='_blank'>
+              <p className='article__date'>2 августа, 2019</p>
+              <h3 className='article__title'>Национальное достояние – парки</h3>
+              <p className='article__text'>В 2016 году Америка отмечала важный юбилей: сто лет назад здесь начала складываться система национальных парков...</p>
+              <p className='article__source'>ЛЕНТА.РУ</p>
+            </a>
+          </div>
         </li>
 
         <li className='article'>
           <div className='article__image' style={{ backgroundImage: `url(${sampleImage})` }} />
-          <p className='article__date'>2 августа, 2019</p>
-          <h3 className='article__title'>Национальное достояние – парки</h3>
-          <p className='article__text'>В 2016 году Америка отмечала важный юбилей: сто лет назад здесь начала складываться система национальных парков...</p>
-          <p className='article__source'>РИА</p>
+          <div className='article__wrapper'>
+            <a className='article__link' href='#' target='_blank'>
+              <p className='article__date'>2 августа, 2019</p>
+              <h3 className='article__title'>Национальное достояние – парки</h3>
+              <p className='article__text'>В 2016 году Америка отмечала важный юбилей: сто лет назад здесь начала складываться система национальных парков...</p>
+              <p className='article__source'>ЛЕНТА.РУ</p>
+            </a>
+          </div>
         </li>
       </ul>
       <button className='search-results__showmore-button'>Показать еще</button>
